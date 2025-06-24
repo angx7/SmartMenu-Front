c8a860c4275c6d9460cfac8165100fc6b0ece497
Kuripipeer <mchristianaxel@gmail.com>
2025-06-24 12:21:29 -0600

    Terminación detalle pedido

---
+++ Models/ComandaPedido.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace SmartMenu.Models
+{
+    public class ComandaPedido
+    {
+        public int pedido_id { get; set; }
+        public int mesa_id { get; set; }
+        public string estado { get; set; }
+        public string fecha { get; set; }
+        public string mesero { get; set; }
+        public List<ComandaDetalle> comanda { get; set; }
+    }
+
+    public class ComandaDetalle
+    {
+        public int pedido_id { get; set; }
+        public string platillo { get; set; }
+        public int cantidad { get; set; }
+    }
+}
---
+++ ViewModels/PedidosViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SmartMenu.Models;
+using Microsoft.Maui.Storage;
+
+namespace SmartMenu.ViewModels
+{
+    public class PedidosViewModel
+    {
+        public ObservableCollection<ComandaPedido> Pedidos { get; set; } = new();
+        public bool IsBusy { get; set; }
+
+        public PedidosViewModel()
+        {
+            _ = CargarPedidos();
+        }
+
+        public async Task CargarPedidos()
+        {
+            if (IsBusy) return;
+            IsBusy = true;
+            try
+            {
+                var token = Preferences.Get("token", null);
+                if (string.IsNullOrWhiteSpace(token)) return;
+
+                var baseUrl = Environment.GetEnvironmentVariable("NGROK_URL");
+                using var client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var response = await client.GetAsync($"{baseUrl}/api/pedidos/comandas");
+                var json = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode) return;
+
+                var pedidos = JsonConvert.DeserializeObject<List<ComandaPedido>>(json);
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Pedidos.Clear();
+                    foreach (var p in pedidos)
+                        Pedidos.Add(p);
+                });
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
